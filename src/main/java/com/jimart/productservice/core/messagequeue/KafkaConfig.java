2718ccbeb66b3e62e21872c8e897920f7d520a17
jiwon615 <seg615@naver.com>
2023-06-03 19:04:00 +0900

    feat: KafkaConfig 등록

---
+++ src/main/java/com/jimart/productservice/core/messagequeue/KafkaConfig.java
@@ -0,0 +1,40 @@
+package com.jimart.productservice.core.messagequeue;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+    // 접속 정보 등록
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(GROUP_ID_CONFIG, "consumerGroupId");
+        properties.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(properties);
+    }
+
+    // 접속 정보를 이용한 리스너 등록
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> containerFactory = new ConcurrentKafkaListenerContainerFactory<>();
+        containerFactory.setConsumerFactory(consumerFactory());
+        return containerFactory;
+    }
+
+}
