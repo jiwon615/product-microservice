f8917aa0d68c29bf6279695ab8aea6016e35c49c
jiwon615 <seg615@naver.com>
2023-06-16 21:18:14 +0900

    modify: yml

--- src/main/java/com/jimart/productservice/domain/product/controller/ProductTestController.java
+++ src/main/java/com/jimart/productservice/domain/product/controller/ProductTestController.java
@@ -1,6 +1,7 @@
 package com.jimart.productservice.domain.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product-service")
 @RequiredArgsConstructor
+@Profile("!prod")
 public class ProductTestController {
 
     private final Environment env;
@@ -17,5 +19,4 @@ public class ProductTestController {
     public String status() {
         return String.format("Product Service is Working on PORT %s", env.getProperty("local.server.port"));
     }
-
 }
